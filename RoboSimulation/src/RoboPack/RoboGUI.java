fff3ab602f0f446277e71905fb1a9caa14160fa3
Abraxas168 <>
2023-12-07 19:28:34 +0100

    GUI form erstellt. nur herumprobiert. vorerst in test branch

---
+++ RoboSimulation/src/RoboPack/RoboGUI.java
@@ -0,0 +1,14 @@
+package RoboPack;
+
+import javax.swing.*;
+
+public class RoboGUI {
+    private JPanel Robo_der_Roboter;
+    private JButton bEingabe;
+    private JTextArea text;
+    private JTextField Eingabefeld;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
