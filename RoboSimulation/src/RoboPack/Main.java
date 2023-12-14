b962d1e931aa4ece10abc7b0840e1779db524576
Abraxas168 <>
2023-12-14 15:14:18 +0100

    Hallo Welt

--- RoboSimulation/src/RoboPack/Main.java
+++ RoboSimulation/src/RoboPack/Main.java
@@ -2,6 +2,6 @@ package RoboPack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hallo Welt");
     }
 }
