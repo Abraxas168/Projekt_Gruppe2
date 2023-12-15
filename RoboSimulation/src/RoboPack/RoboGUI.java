dd59fd4cf2d5765db01639b14a81397cbc9e54de
Sarah Jenewein <jenesa01@thu.de>
2023-12-15 18:34:13 +0100

    Erster GUI Entwurf

--- RoboSimulation/src/RoboPack/RoboGUI.java
+++ RoboSimulation/src/RoboPack/RoboGUI.java
@@ -1,4 +1,13 @@
 package RoboPack;
 
+import javax.swing.*;
+
 public class RoboGUI {
+    private JSlider sRadius;
+    private JLabel lRadius;
+    private JButton bOben;
+    private JButton bUnten;
+    private JButton bLinks;
+    private JButton rechtsButton;
+    private JTextArea tRoboinfo;
 }
