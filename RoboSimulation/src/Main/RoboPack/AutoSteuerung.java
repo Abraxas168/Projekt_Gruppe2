6fb73cedb9e3fa4ed8fd8f84e21ba4c08090ec56
Abraxas168 <>
2024-01-04 22:17:51 +0100

    versuch

--- RoboSimulation/src/Main/RoboPack/AutoSteuerung.java
+++ RoboSimulation/src/Main/RoboPack/AutoSteuerung.java
@@ -22,8 +22,9 @@ public class AutoSteuerung extends Steuerung{
         int velocity=robo.getVelocity();
         double orientation=robo.getOrientation();
         //*************Berechnung mit Sensordaten!
-        if (datafromSensors != null && datafromSensors.size()>0){
-        System.out.println(""+datafromSensors.get(0).getAngle());}
+       if (datafromSensors != null && datafromSensors.size()>0){
+        //System.out.println(""+datafromSensors.get(0).getAngle());
+        }
         robo.setVelocity(velocity);
         robo.setOrientation(orientation);
     }
