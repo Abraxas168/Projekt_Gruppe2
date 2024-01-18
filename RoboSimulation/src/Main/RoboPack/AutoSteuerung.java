3fd153df215bfe8fd974ff40a1a2cf50139e1dd5
Sarah Jenewein <jenesa01@thu.de>
2024-01-18 21:10:10 +0100

    Robot sets his orientation towards the targetzone if the velocity=50 and 4s have passed

--- RoboSimulation/src/Main/RoboPack/AutoSteuerung.java
+++ RoboSimulation/src/Main/RoboPack/AutoSteuerung.java
@@ -7,21 +7,28 @@ public class AutoSteuerung extends Steuerung implements IObserver{
     private List<List<SensorData>> sensorData=new ArrayList<>();
 
     private int gelesen;
-
+    private long lastAlignmentTime;
+    private static final long ALIGNMENT_INTERVAL = 4000;
 
     AutoSteuerung(){
         this.gelesen=0;
+        this.lastAlignmentTime = System.currentTimeMillis();
     }
     @Override
     public void steuern(Roboter robo){
         int velocity=robo.getVelocity();
         robo.setVelocity(50);
-        double orientation= robo.getOrientation();
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastAlignmentTime >= ALIGNMENT_INTERVAL && velocity == 50) {
+            robo.setOrientation(0.0);
+            lastAlignmentTime = currentTime;
+        }
+        double orientation = robo.getOrientation();
         int n=this.gelesen;
         if (sensorData !=null && sensorData.size()>0){
             while(n< sensorData.size()) {
                 List<SensorData> daten=sensorData.get(n);
-                System.out.println(daten.size()+ "");
+                //System.out.println(daten.size()+ "");
                 for (int j = 0; j < daten.size(); j++) {
                     SensorData sensorData1=daten.get(j);
                     BaseSensor relatedSensor = sensorData1.getRelatedSensor();
@@ -37,7 +44,7 @@ public class AutoSteuerung extends Steuerung implements IObserver{
                     if (distance <= robo.getVelocity()*3+robo.getRadius()) {
                             robo.setVelocity(20);
                         if (relation_toRobo == 0) {
-                            System.out.println("Sensor: 0  - " + angle + "Distance:" + distance);
+                            //System.out.println("Sensor: 0  - " + angle + "Distance:" + distance);
                             if (angle >= 0.0) {
                                 orientation=robo.getOrientation();
                                 robo.setOrientation(orientation - (beamwidth/2-Math.abs(angle)));
@@ -56,7 +63,7 @@ public class AutoSteuerung extends Steuerung implements IObserver{
 
                         }
                         if (relation_toRobo == Math.PI/3) {
-                            System.out.println("Sensor: pi/3 - "+ angle+ "Distance:" + distance);
+                            //System.out.println("Sensor: pi/3 - "+ angle+ "Distance:" + distance);
                             if (angle >= 0.0) {
                                 orientation=robo.getOrientation();
                                 robo.setOrientation(orientation - (beamwidth/2-Math.abs(angle)));
@@ -69,15 +76,13 @@ public class AutoSteuerung extends Steuerung implements IObserver{
                         }
                         if (relation_toRobo == -Math.PI/3) {
 
-                           System.out.println("Sensor: -pi/3:  -" + angle + "Distance:" + distance);
+                           //System.out.println("Sensor: -pi/3:  -" + angle + "Distance:" + distance);
                             if (angle >= 0.0) {
                                orientation=robo.getOrientation();
-                                //robo.setOrientation(orientation+Math.PI/4);
                                 robo.setOrientation(orientation + (beamwidth/2-Math.abs(angle)));
                                 break;
                             } else {
                                 orientation=robo.getOrientation();
-                                //robo.setOrientation(orientation+Math.PI/2);
                                 robo.setOrientation(orientation + (beamwidth/2+Math.abs(angle)));
                                 break;
                             }
@@ -86,11 +91,6 @@ public class AutoSteuerung extends Steuerung implements IObserver{
                 }
             n=n+1;}
         } this.gelesen=sensorData.size();
-        
-        //*************Berechnung mit Sensordaten!
-
-        //robo.setVelocity(velocity);
-        //robo.setOrientation(orientation);
     }
 
     @Override
