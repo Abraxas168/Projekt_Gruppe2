1729f2c43c0296ff8b2778c7a76b3fbfedd17ea2
Abraxas168 <>
2024-01-18 21:38:30 +0100

    Lenkung etwas an neue bedingungen angepasst

--- RoboSimulation/src/Main/RoboPack/AutoSteuerung.java
+++ RoboSimulation/src/Main/RoboPack/AutoSteuerung.java
@@ -36,40 +36,31 @@ public class AutoSteuerung extends Steuerung implements IObserver{
                     double distance = sensorData1.getDistance();
                     double angle = sensorData1.getAngle();
                     double beamwidth = relatedSensor.getBeamWidth();
-                    //System.out.println(beamwidth);
-                    //System.out.println(angle);
-                    sensorData1.getX(); //distance
-                    sensorData1.getY(); //distance
-
                     if (distance <= robo.getVelocity()*3+robo.getRadius()) {
                             robo.setVelocity(20);
                         if (relation_toRobo == 0) {
                             System.out.println("Sensor: 0  - " + angle + "Distance:" + distance);
                             if (angle >= 0.0) {
-                                orientation=robo.getOrientation();
                                 robo.setOrientation(orientation - (beamwidth/2-Math.abs(angle)));
                                 if (beamwidth==Math.PI/5){
-                                    robo.setOrientation(robo.getOrientation()-(2*Math.PI)/6);
+                                    robo.setOrientation(robo.getOrientation()-(2*Math.PI)/5);
                                 }
                                 break;
                             } else {
-                                orientation=robo.getOrientation();
+
                                 robo.setOrientation(orientation + (beamwidth/2-Math.abs(angle)));
                                 if (beamwidth==Math.PI/5){
-                                    robo.setOrientation(robo.getOrientation()+(2*Math.PI)/6);
+                                    robo.setOrientation(robo.getOrientation()+(2*Math.PI)/5);
                                 }
                                 break;
                             }
-
                         }
                         if (relation_toRobo == Math.PI/3) {
                             //System.out.println("Sensor: pi/3 - "+ angle+ "Distance:" + distance);
                             if (angle >= 0.0) {
-                                orientation=robo.getOrientation();
                                 robo.setOrientation(orientation - (beamwidth/2-Math.abs(angle)));
                                 break;
                             } else {
-                                orientation=robo.getOrientation();
                                 robo.setOrientation(orientation - (beamwidth/2+Math.abs(angle)));
                                 break;
                             }
@@ -78,11 +69,9 @@ public class AutoSteuerung extends Steuerung implements IObserver{
 
                            //System.out.println("Sensor: -pi/3:  -" + angle + "Distance:" + distance);
                             if (angle >= 0.0) {
-                               orientation=robo.getOrientation();
                                 robo.setOrientation(orientation + (beamwidth/2-Math.abs(angle)));
                                 break;
                             } else {
-                                orientation=robo.getOrientation();
                                 robo.setOrientation(orientation + (beamwidth/2+Math.abs(angle)));
                                 break;
                             }
@@ -101,7 +90,4 @@ public class AutoSteuerung extends Steuerung implements IObserver{
     public List<List<SensorData>> getDatafromSensors() {
         return sensorData;
     }
-  //  public void setDatafromSensors(List<SensorData> sd){
-   //     this.sensorData.add(sd);
-    //}
 }
