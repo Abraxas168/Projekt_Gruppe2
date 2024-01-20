2f9cee78fd6d7808c739172948b18defa7e00c81
Sarah Jenewein <jenesa01@thu.de>
2024-01-20 19:11:59 +0100

    add:Tests, testing deceleration and acceleration is not succesfull so far

--- RoboSimulation/src/Test/Test/RoboterTest.java
+++ RoboSimulation/src/Test/Test/RoboterTest.java
@@ -1,6 +1,7 @@
 package Test;
 
 import org.junit.jupiter.api.Test;
+import thu.robots.components.IRobot;
 import thu.robots.components.Roboter;
 import thu.robots.components.Sensor;
 import thu.robots.components.Steuerung;
@@ -26,17 +27,50 @@ class RoboterTest {
 
     @Test
     void accelerate() {
+        Roboter robot = new Roboter(new Sensor(-Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/3, 40),
+                new Sensor(Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/5, 30),
+                "TestRoboter", 0, 20, Color.RED, new Steuerung());
+
+        robot.accelerate(30);
+        assertEquals(30, robot.getVelocity());
+
+        robot.accelerate(IRobot.MAX_VELOCITY + 10);
+        assertEquals(IRobot.MAX_VELOCITY, robot.getVelocity());
     }
 
     @Test
     void decelerate() {
+        Roboter robot = new Roboter(new Sensor(-Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/3, 40),
+                new Sensor(Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/5, 30),
+                "TestRoboter", 0, 20, Color.RED, new Steuerung());
+
+        robot.decelerate(30);
+        assertEquals(30, robot.getVelocity());
+
+        robot.decelerate(-10);
+        assertEquals(0, robot.getVelocity());
     }
 
     @Test
     void setRadius() {
+        Roboter robot = new Roboter(new Sensor(-Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/3, 40),
+                new Sensor(Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/5, 30),
+                "TestRoboter", 0, 20, Color.RED, new Steuerung());
+
+        robot.setRadius(20);
+        assertEquals(20, robot.getRadius());
+        assertThrows(IllegalStateException.class, () -> robot.setRadius(150));
     }
 
     @Test
     void move() {
+        Roboter robot = new Roboter(new Sensor(-Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/3, 40),
+                new Sensor(Math.PI/3, Math.PI/3, 40), new Sensor(0, Math.PI/5, 30),
+                "TestRoboter", 0, 20, Color.RED, new Steuerung());
+        robot.setVelocity(30);
+        robot.move(1.0);
+
+        assertEquals(30, robot.getPosX());
+        assertEquals(0, robot.getPosY());
     }
 }
