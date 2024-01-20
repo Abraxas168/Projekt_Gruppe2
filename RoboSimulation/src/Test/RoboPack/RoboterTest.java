29817fd5d117563cb197b3697b912e26cc35c91b
Sarah Jenewein <jenesa01@thu.de>
2024-01-20 11:08:37 +0100

    add test for Class Roboter

---
+++ RoboSimulation/src/Test/RoboPack/RoboterTest.java
@@ -0,0 +1,28 @@
+package RoboPack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoboterTest {
+
+    @Test
+    void normalizeOrientation(){
+    }
+
+    @Test
+    void accelerate() {
+    }
+
+    @Test
+    void decelerate() {
+    }
+
+    @Test
+    void setRadius() {
+    }
+
+    @Test
+    void move() {
+    }
+}
